a276b135d37f900e4360c76e84545d9dfc731b46
Ricardo Silvério <ricardo.silverio.souza@gmail.com>
2018-05-31 12:26:43 -0300

    Usando o banco e incluindo usuários iniciais

--- src/index.js
+++ src/index.js
@@ -1,33 +1,69 @@
-const { GraphQLServer } = require('graphql-yoga')
+const {GraphQLServer} = require('graphql-yoga')
+const uuid = require('uuid').v4
+
+const {MongoClient} = require('mongodb')
+const dbUrl = 'mongodb://localhost:27017/'
+let hackerNewsDb, linksCollection, usersCollection
+MongoClient.connect(dbUrl)
+.then(dbObj => {
+  hackerNewsDb = dbObj.db('hackernews')
+  linksCollection = hackerNewsDb.collection('link')
+  usersCollection = hackerNewsDb.collection('user')
+  return restartDB()
+})
+.then(() => console.log('Database connected'))
+
+const restartDB = () => {
+  linksCollection.deleteMany({})
+  usersCollection.deleteMany({})
+  return insertUsers()
+}
+
+const extractSingleResult = results => results.length > 0 ? results[0] : {}
 
 const typeDefs = './src/schema.graphql'
 
-let links = [{
-  id: new Date().valueOf().toString(),
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
+const insertUsers = () => {
+  const bob = {id: uuid(), name: 'Bob', email: 'bob@gmail.com', links: []}
+  const alice = {id: uuid(), name: 'Alice', email: 'alice@gmail.com', links: []}
+  return usersCollection.insertMany([bob, alice])
+  .then(() => insertLink('www.myblog.com', 'My personal Blog', bob).then(link => updateUser(bob.id, link)))
+  .then(() => insertLink('www.myrecipes.com', 'My recipes Site', alice).then(link => updateUser(alice.id, link)))
+}
+
+const findUser = id => usersCollection.find({id}).toArray().then(extractSingleResult)
+
+const updateUser = (id, link) => {
+  return findUser(id)
+  .then(user => usersCollection.updateOne({id}, {$set: {links: [...user.links, link]}}))
+}
+
+const findAllUsers = () => usersCollection.find({}).toArray()
+
+const insertLink = (url, description, postedBy) => {
+  const link = {url, description, id: uuid(), postedBy}
+  return linksCollection.insertOne(link).then(results => results.ops[0])
+}
+
+const findLink = id => {
+  const query = {id}
+  return linksCollection.find(query).toArray()
+  .then(extractSingleResult)
+}
+
+const findAllLinks = () => linksCollection.find({}).toArray()
 
 const resolvers = {
   Query: {
     info: () => null,
-    feed: () => links,
-    link: id => links.find(l => l.id === id)
+    feed: () => findAllLinks(),
+    link: (root, args) => findLink(args.id),
+    users: () => findAllUsers()
   },
   Mutation: {
     post: (root, args) => {
       const {url, description} = args
-      const link = {url, description, id: new Date().valueOf().toString()}
-      links.push(link)
-      return link
-    },
-    update: (root, args) => {
-      const {url, description, id} = args
-      links = links.map(l => l.id === id ? {url, description, id} : l)
-    },
-    delete: (root, args) => {
-      const {id} = args
-      links = links.filter(l => l.id !== id)
+      return insertLink(url, description)
     }
   }
 }
